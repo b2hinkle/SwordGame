f5c7ac73bbed0e015250d59c80f20ba2af6bb711
Christian Hinkle <christianhinko@gmail.com>
2025-01-20 02:27:21 -0700

    [Source] Use `bReplicates` instead of `SetReplicates()` for constructor code to fix warning.

--- Source/SwordGame/Private/RotatableMap.cpp
+++ Source/SwordGame/Private/RotatableMap.cpp
@@ -13,7 +13,7 @@ ARotatableMap::ARotatableMap()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	SetReplicates(true);
+	bReplicates = true;
 	AngleAxis = 0;
 
 }
