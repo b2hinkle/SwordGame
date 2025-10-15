45f8793f3f152a33c88e705825c8395f94f69889
b2hinkle <b2hinkle@gmail.com>
2025-10-14 21:36:03 -0600

    Allow logging in shipping

--- Source/SwordGame.Target.cs
+++ Source/SwordGame.Target.cs
@@ -14,6 +14,7 @@ public class SwordGameTarget : TargetRules
         {
             bUseConsoleInShipping = true;
             bUseExecCommandsInShipping = true;
+            bUseLoggingInShipping = true;
         }
     }
 }
